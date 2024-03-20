0c08a4570011d3b9d20887cbb10e719c5218ae9e
Muhammad Moinuddin <moinuddin010203@gmail.com>
2024-03-20 12:08:20 +0500

    completed event handling in detail by using ES5 and Es6 and by using fucntion declaration and by function expression. If the fucntion is starting withconst rather than functin it means it is a fucntion expression

--- Meta React Course/2ndweekevents/src/App.js
+++ Meta React Course/2ndweekevents/src/App.js
@@ -1,9 +1,9 @@
-import Btn from "./Components/Btn";
+// import Btn from "./Components/Btn";
 
 // const clickHandler = () => {
 //   console.log('clicked');
 // }
-    
+
 // function App() {
 //   return (
 //     <button onClick={clickHandler}>CLick Me</button>
@@ -13,3 +13,56 @@ import Btn from "./Components/Btn";
 // export default App;
 
 // Handling events using inline anonymous ES5 functions
+function App() {
+    return (
+        <button onClick={function () { console.log('first - example') }}>
+            An anonymous function in ES5
+        </button>
+    );
+}
+// export default App;
+
+// Handling events using inline anonymous ES6 functions (arrow functions)
+function ES6() {
+    return (
+        <button onClick={() => console.log('second - example')}>
+            An anonymous function in ES6
+        </button>
+    );
+}
+// export default ES6;
+
+// Handling events using separate function declarations
+
+function separateFuncDec() {
+    function thirdExapmle() {
+        console.log('thirdExapmlehe babu');
+    }
+    return (
+        <div className="thirdExample">
+            <button onClick={thirdExapmle}>
+                Using a eparate funciton declaration
+            </button>
+        </div>
+    );
+}
+
+// export default separateFuncDec;
+
+// Handling events using separate function expressions
+
+function separateFuncExp() {
+    const fourthExample = () =>
+        console.log('fourth example he by using arrow func');
+    {
+        return (
+            <div className="fourthExample">
+                <button onClick={fourthExample}>
+                    using a separate function expression
+                </button>
+            </div>
+        );
+    }
+}
+
+export default separateFuncExp;
