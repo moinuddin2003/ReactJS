ed21b2911364b870313a1d7b8619d76c12ac65f2
Muhammad Moinuddin <moinuddin010203@gmail.com>
2024-03-20 11:29:31 +0500

    Changed the color of the comment in settings .json

--- Meta React Course/2ndweekevents/src/App.js
+++ Meta React Course/2ndweekevents/src/App.js
@@ -1,13 +1,15 @@
 import Btn from "./Components/Btn";
 
-const clickHandler = () => {
-  console.log('clicked');
-}
+// const clickHandler = () => {
+//   console.log('clicked');
+// }
     
-function App() {
-  return (
-    <button onClick={clickHandler}>CLick Me</button>
-  );
-}
+// function App() {
+//   return (
+//     <button onClick={clickHandler}>CLick Me</button>
+//   );
+// }
 
-export default App;
+// export default App;
+
+// Handling events using inline anonymous ES5 functions
