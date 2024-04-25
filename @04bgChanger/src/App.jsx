c44046ea0ab45a4a7f89fa42427817170a9bf6cf
Muhammad Moinuddin <moinuddin010203@gmail.com>
2024-04-25 16:51:08 +0500

    When Clicking on the button it changes the color of the page

--- @04bgChanger/src/App.jsx
+++ @04bgChanger/src/App.jsx
@@ -4,16 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("green")
+  const [color, setColor] = useState('olive')
   return (
     <div className='w-full h-screen duration-200'
       style = {{backgroundColor: color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center bg-white px-3 py-2 rounded-3xl'>
-        <button className='shadow-lg rounded-full outline-none text-white px-4 py-1'
+        <button onClick = {() => setColor("blue")} className='shadow-lg rounded-full outline-none text-white px-4 py-1'
         style = {{backgroundColor: 'blue'}}
         >Blue</button>
-
         </div>
       </div>
     </div>
