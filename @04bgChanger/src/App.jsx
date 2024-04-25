13ca27ed6f7ef6d63c8ad940c559001babe3ac47
Muhammad Moinuddin <moinuddin010203@gmail.com>
2024-04-25 16:33:13 +0500

    Making bgChnager 1st button is completed

---
+++ @04bgChanger/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("green")
+  return (
+    <div className='w-full h-screen duration-200'
+      style = {{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center bg-white px-3 py-2 rounded-3xl'>
+        <button className='shadow-lg rounded-full outline-none text-white px-4 py-1'
+        style = {{backgroundColor: 'blue'}}
+        >Blue</button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+export default App
