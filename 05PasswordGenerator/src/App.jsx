7fe7a508d878fe7deeb25467a1208f4177385ddd
Muhammad Moinuddin <moinuddin010203@gmail.com>
2024-04-28 00:22:22 +0500

    Some UI changes

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -43,11 +43,11 @@ function App() {
 
 
   return (
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-center text-orange-500 bg-gray-700'>Password Generator
-      <div className='flex shadow rounded-lg overflow-hidden mb-4' >
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-6 mt-10 text-center text-white bg-gray-600'>Password Generator
+      <div className='flex shadow rounded-lg overflow-hidden mt-4 mb-4' >
         <input type='text'
           value={password}
-          className='outline-none w-full py-1 px-3'
+          className='outline-none text-orange-500 w-full  py-1 px-3'
           placeholder='Password'
           readOnly
           ref={passwordRef}
@@ -56,7 +56,7 @@ function App() {
           className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
       </div>
       <div className='flex text-sm gap-x-2'>
-        <div className='flex items-center gap-x-1'>
+        <div className='flex text-orange-500 items-center gap-x-1'>
           <input
             type="range"
             min={8}
@@ -67,7 +67,7 @@ function App() {
           />
           <label> Length: {length}</label>
         </div>
-        <div className='flex items-center gap-x-1'>
+        <div className='flex text-orange-500 items-center gap-x-1'>
           <input
             type='checkbox'
             defaultChecked={numberAllowed}
@@ -78,7 +78,7 @@ function App() {
           />
           <label htmlFor="numberInput">Numbers</label>
         </div>
-        <div className='flex items-center gap-x-1'>
+        <div className='flex text-orange-500 items-center gap-x-1'>
           <input
             type='checkbox'
             defaultChecked={characterAllowed}
